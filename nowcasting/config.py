c6cd2ae64439051b18380600d815fc684b0b1f96
CHANDRA SHEKAR LAKSHMINARAYANAN <chandru@CHANDRAs-MacBook-Pro.local>
2019-06-06 19:31:42 -0600

    removing hko from config.py

--- nowcasting/config.py
+++ nowcasting/config.py
@@ -26,75 +26,75 @@ if not os.path.exists(__C.MNIST_PATH):
 __C.HKO_DATA_BASE_PATH = os.path.join(__C.ROOT_DIR, 'hko_data')
 
 # Append your path to the possible paths
-possible_hko_png_paths = [os.path.join('E:\\datasets\\HKO-data\\radarPNG\\radarPNG'),
-                          os.path.join(__C.HKO_DATA_BASE_PATH, 'radarPNG')]
-possible_hko_mask_paths = [os.path.join('E:\\datasets\\HKO-data\\radarPNG\\radarPNG_mask'),
-                           os.path.join(__C.HKO_DATA_BASE_PATH, 'radarPNG_mask')]
-# Search for the radarPNG
-find_hko_png_path = False
-for ele in possible_hko_png_paths:
-    if os.path.exists(ele):
-        find_hko_png_path = True
-        __C.HKO_PNG_PATH = ele
-        break
-if not find_hko_png_path:
-    raise RuntimeError("radarPNG is not found! You can download the radarPNG using"
-                       " `bash download_radar_png.bash`")
-# Search for the radarPNG_mask
-find_hko_mask_path = False
-for ele in possible_hko_mask_paths:
-    if os.path.exists(ele):
-        find_hko_mask_path = True
-        __C.HKO_MASK_PATH = ele
-        break
-if not find_hko_mask_path:
-    raise RuntimeError("radarPNG_mask is not found! You can download the radarPNG_mask using"
-                       " `bash download_radar_png.bash`")
-if not os.path.exists(__C.HKO_DATA_BASE_PATH):
-    os.makedirs(__C.HKO_DATA_BASE_PATH)
-__C.HKO_PD_BASE_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'pd')
-if not os.path.exists(__C.HKO_PD_BASE_PATH):
-    os.makedirs(__C.HKO_PD_BASE_PATH)
-__C.HKO_VALID_DATETIME_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'valid_datetime.pkl')
-__C.HKO_SORTED_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'sorted_day.pkl')
-__C.HKO_RAINY_TRAIN_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'hko7_rainy_train_days.txt')
-__C.HKO_RAINY_VALID_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'hko7_rainy_valid_days.txt')
-__C.HKO_RAINY_TEST_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'hko7_rainy_test_days.txt')
+# possible_hko_png_paths = [os.path.join('E:\\datasets\\HKO-data\\radarPNG\\radarPNG'),
+#                           os.path.join(__C.HKO_DATA_BASE_PATH, 'radarPNG')]
+# possible_hko_mask_paths = [os.path.join('E:\\datasets\\HKO-data\\radarPNG\\radarPNG_mask'),
+#                            os.path.join(__C.HKO_DATA_BASE_PATH, 'radarPNG_mask')]
+# # Search for the radarPNG
+# find_hko_png_path = False
+# for ele in possible_hko_png_paths:
+#     if os.path.exists(ele):
+#         find_hko_png_path = True
+#         __C.HKO_PNG_PATH = ele
+#         break
+# if not find_hko_png_path:
+#     raise RuntimeError("radarPNG is not found! You can download the radarPNG using"
+#                        " `bash download_radar_png.bash`")
+# # Search for the radarPNG_mask
+# find_hko_mask_path = False
+# for ele in possible_hko_mask_paths:
+#     if os.path.exists(ele):
+#         find_hko_mask_path = True
+#         __C.HKO_MASK_PATH = ele
+#         break
+# if not find_hko_mask_path:
+#     raise RuntimeError("radarPNG_mask is not found! You can download the radarPNG_mask using"
+#                        " `bash download_radar_png.bash`")
+# if not os.path.exists(__C.HKO_DATA_BASE_PATH):
+#     os.makedirs(__C.HKO_DATA_BASE_PATH)
+# __C.HKO_PD_BASE_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'pd')
+# if not os.path.exists(__C.HKO_PD_BASE_PATH):
+#     os.makedirs(__C.HKO_PD_BASE_PATH)
+# __C.HKO_VALID_DATETIME_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'valid_datetime.pkl')
+# __C.HKO_SORTED_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'sorted_day.pkl')
+# __C.HKO_RAINY_TRAIN_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'hko7_rainy_train_days.txt')
+# __C.HKO_RAINY_VALID_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'hko7_rainy_valid_days.txt')
+# __C.HKO_RAINY_TEST_DAYS_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'hko7_rainy_test_days.txt')
 
-__C.HKO_PD = edict()
-__C.HKO_PD.ALL = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_all.pkl')
-__C.HKO_PD.ALL_09_14 = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_all_09_14.pkl')
-__C.HKO_PD.ALL_15 = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_all_15.pkl')
-__C.HKO_PD.RAINY_TRAIN = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_rainy_train.pkl')
-__C.HKO_PD.RAINY_VALID = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_rainy_valid.pkl')
-__C.HKO_PD.RAINY_TEST = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_rainy_test.pkl')
+# __C.HKO_PD = edict()
+# __C.HKO_PD.ALL = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_all.pkl')
+# __C.HKO_PD.ALL_09_14 = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_all_09_14.pkl')
+# __C.HKO_PD.ALL_15 = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_all_15.pkl')
+# __C.HKO_PD.RAINY_TRAIN = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_rainy_train.pkl')
+# __C.HKO_PD.RAINY_VALID = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_rainy_valid.pkl')
+# __C.HKO_PD.RAINY_TEST = os.path.join(__C.HKO_PD_BASE_PATH, 'hko7_rainy_test.pkl')
 
-__C.HKO = edict()
-__C.HKO.ITERATOR = edict()
-__C.HKO.ITERATOR.WIDTH = 480
-__C.HKO.ITERATOR.HEIGHT = 480
-__C.HKO.ITERATOR.FILTER_RAINFALL = True           # Whether to discard part of the rainfall, has a denoising effect
-__C.HKO.ITERATOR.FILTER_RAINFALL_THRESHOLD = 0.28 # All the pixel values that are smaller than round(threshold * 255) will be discarded
+# __C.HKO = edict()
+# __C.HKO.ITERATOR = edict()
+# __C.HKO.ITERATOR.WIDTH = 480
+# __C.HKO.ITERATOR.HEIGHT = 480
+# __C.HKO.ITERATOR.FILTER_RAINFALL = True           # Whether to discard part of the rainfall, has a denoising effect
+# __C.HKO.ITERATOR.FILTER_RAINFALL_THRESHOLD = 0.28 # All the pixel values that are smaller than round(threshold * 255) will be discarded
 
 
 # The Benchmark parameters
-__C.HKO.BENCHMARK = edict()
-__C.HKO.BENCHMARK.STAT_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'benchmark_stat')
-if not os.path.exists(__C.HKO.BENCHMARK.STAT_PATH):
-    os.makedirs(__C.HKO.BENCHMARK.STAT_PATH)
-__C.HKO.BENCHMARK.VISUALIZE_SEQ_NUM = 10  # Number of sequences that will be plotted and saved to the benchmark directory
-__C.HKO.BENCHMARK.IN_LEN = 5   # The maximum input length to ensure that all models are tested on the same set of input data
-__C.HKO.BENCHMARK.OUT_LEN = 20  # The maximum output length to ensure that all models are tested on the same set of input data
-__C.HKO.BENCHMARK.STRIDE = 5   # The stride
+# __C.HKO.BENCHMARK = edict()
+# __C.HKO.BENCHMARK.STAT_PATH = os.path.join(__C.HKO_DATA_BASE_PATH, 'benchmark_stat')
+# if not os.path.exists(__C.HKO.BENCHMARK.STAT_PATH):
+#     os.makedirs(__C.HKO.BENCHMARK.STAT_PATH)
+# __C.HKO.BENCHMARK.VISUALIZE_SEQ_NUM = 10  # Number of sequences that will be plotted and saved to the benchmark directory
+# __C.HKO.BENCHMARK.IN_LEN = 5   # The maximum input length to ensure that all models are tested on the same set of input data
+# __C.HKO.BENCHMARK.OUT_LEN = 20  # The maximum output length to ensure that all models are tested on the same set of input data
+# __C.HKO.BENCHMARK.STRIDE = 5   # The stride
 
 
-__C.HKO.EVALUATION = edict()
-__C.HKO.EVALUATION.ZR = edict()
-__C.HKO.EVALUATION.ZR.a = 58.53  # The a factor in the Z-R relationship
-__C.HKO.EVALUATION.ZR.b = 1.56  # The b factor in the Z-R relationship
-__C.HKO.EVALUATION.THRESHOLDS = (0.5, 2, 5, 10, 30)
-__C.HKO.EVALUATION.BALANCING_WEIGHTS = (1, 1, 2, 5, 10, 30)  # The corresponding balancing weights
-__C.HKO.EVALUATION.CENTRAL_REGION = (120, 120, 360, 360)
+# __C.HKO.EVALUATION = edict()
+# __C.HKO.EVALUATION.ZR = edict()
+# __C.HKO.EVALUATION.ZR.a = 58.53  # The a factor in the Z-R relationship
+# __C.HKO.EVALUATION.ZR.b = 1.56  # The b factor in the Z-R relationship
+# __C.HKO.EVALUATION.THRESHOLDS = (0.5, 2, 5, 10, 30)
+# __C.HKO.EVALUATION.BALANCING_WEIGHTS = (1, 1, 2, 5, 10, 30)  # The corresponding balancing weights
+# __C.HKO.EVALUATION.CENTRAL_REGION = (120, 120, 360, 360)
 
 __C.MOVINGMNIST = edict()
 __C.MOVINGMNIST.DISTRACTOR_NUM = 0
